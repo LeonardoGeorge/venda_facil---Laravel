0aa47d58f1e5a3dd805d36c8848a9c269015b76d
Leonardo George <leonardogeorge30@gmail.com>
2025-08-30 10:23:19 -0300

    up migration

--- venda_facil-Laravel/app/Models/Cliente.php
+++ venda_facil-Laravel/app/Models/Cliente.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $fillable = ['nome', 'telefone', 'email', 'cpf'];
+    protected $fillable = ['nome', 'telefone', 'email', 'cpf', 'endereco'];
     public $timestamps = false;
 }
 
